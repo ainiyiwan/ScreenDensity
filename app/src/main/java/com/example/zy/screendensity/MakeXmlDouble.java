e4a85ddb0009388bba0a1a8d185e140f649a3560
Administrator <13025166246@163.com>
2018-01-18 14:24:38 +0800

    commit MakeXmlDouble

--- app/src/main/java/com/example/zy/screendensity/MakeXmlDouble.java
+++ app/src/main/java/com/example/zy/screendensity/MakeXmlDouble.java
@@ -5,13 +5,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
-public class MakeXmls {
+public class MakeXmlDouble {
 
     //文件夹样式values-w{0}dp
     private final static String rootPaths = "C:\\Users\\Administrator\\Desktop\\layout\\values-w{0}dp\\";
 
     //基准宽度360f
-    private final static float dx = 360f;
+    private final static double dx = 360f;
 
     //基准样式<dimen name="base1dp">1.00dp</dimen>
     private final static String XTemplate = "<dimen name=\"base{0}dp\">{1}dp</dimen>\n";
@@ -27,11 +27,11 @@ public class MakeXmls {
         StringBuffer sb = new StringBuffer();
         sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
         sb.append("<resources>");
-        float cellx = x / dx;//360/360基准数
+        double cellx = x / dx;//360/360基准数
         //打印多少个dp
         for (int i = 1; i < 100; i++) {
             sb.append(XTemplate.replace("{0}", i + "").replace("{1}",
-                    change(cellx * i) + ""));
+                    formatDouble1(cellx * i) + ""));
         }
         sb.append("</resources>");
 
@@ -54,6 +54,15 @@ public class MakeXmls {
     public static float change(float a) {
         int temp = (int) (a * 100);
         return temp / 100.00f;
+    }
+
+    /**
+     * 保留两位小数，四舍五入的一个老土的方法
+     * @param d
+     * @return
+     */
+    public static double formatDouble1(double d) {
+        return (double)Math.round(d*100)/100;
     }
 
 }
